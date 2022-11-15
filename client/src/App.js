28d8eb3edfde17b1baa03d4f53f9c6390a2939d4
mhalkaaf <muhammadhaeckal11@gmail.com>
2022-11-15 11:51:57 +0700

    edit small change in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Muhammad Haeckal Al Kaaf Learn React.js
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
